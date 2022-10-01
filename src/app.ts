e9e902ac7a61b9bf679e8f667092e921fa025c32
José Rodrigues <jose15.luis00@gmail.com>
2022-10-02 00:57:17 +0100

    chore: remove tests

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import "dotenv/config";
+import express, { Express, NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import cors from "cors";
+import { routes } from "./routes";
+import { CustomError } from "./exceptions/CustomError";
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.use(
+  (err: any, _request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof CustomError) {
+      return response
+        .status(err.status)
+        .json({
+          error: err.message,
+        })
+        .end();
+    }
+
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response
+      .status(500)
+      .json({
+        message: "Internal server error!",
+      })
+      .end();
+  }
+);
+
+export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,4 @@
-import "dotenv/config";
-import express, { Express, NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import cors from "cors";
-import { routes } from "./routes";
-import { CustomError } from "./exceptions/CustomError";
+import { app } from "./app";
 import { environment } from "./environment";
-
-const app: Express = express();
-
-app.use(express.json());
-app.use(cors());
-app.use(routes);
-
-app.use(
-  (err: any, _request: Request, response: Response, _next: NextFunction) => {
-    if (err instanceof CustomError) {
-      return response
-        .status(err.status)
-        .json({
-          error: err.message,
-        })
-        .end();
-    }
-
-    if (err instanceof Error) {
-      return response.status(400).json({
-        error: err.message,
-      });
-    }
-
-    return response
-      .status(500)
-      .json({
-        message: "Internal server error!",
-      })
-      .end();
-  }
-);
 
 app.listen(environment.PORT, (): void => console.log("Server running"));
