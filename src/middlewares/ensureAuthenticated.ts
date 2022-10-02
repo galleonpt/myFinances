615aeddb4abe6e5f7e935acf59d6810803daf765
José Rodrigues <jose15.luis00@gmail.com>
2022-10-02 03:21:23 +0100

    feat: add ensure authenticated middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { JwtPayload, verify } from "jsonwebtoken";
+
+export function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authToken: string | undefined = request.headers.authorization;
+
+  if (!authToken) return response.status(401).end();
+
+  const [, token] = authToken.split(" ");
+
+  try {
+    if (process.env.JWT_SECRET) {
+      const info: string | JwtPayload = verify(token, process.env.JWT_SECRET);
+
+      if (info.sub) request.id = +info.sub;
+      if (info.email) request.email = info.email;
+
+      return next();
+    }
+  } catch (e) {
+    console.log(e);
+    return response.status(401).end();
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -4,13 +4,14 @@ import { CreateUserController } from "./controllers/users/CreateUserController";
 import { DeleteUserController } from "./controllers/users/DeleteUserController";
 import { ListSingleUserController } from "./controllers/users/ListSingleUserController";
 import { ListUsersController } from "./controllers/users/ListUsersController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const routes: Router = Router();
 
 routes.post("/login", new LoginController().handle);
 
 routes.post("/users", new CreateUserController().handle);
-routes.get("/users", new ListUsersController().handle);
+routes.get("/users", ensureAuthenticated, new ListUsersController().handle);
 routes.get("/users/:id", new ListSingleUserController().handle);
 routes.post("/users/:id", new DeleteUserController().handle);
 
